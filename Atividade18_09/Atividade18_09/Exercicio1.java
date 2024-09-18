44ce910dcad62ab1dfa7233f5dc1ba971ed8d505
PedroTersiSouza <166923274+PedroTersiSouza@users.noreply.github.com>
2024-09-18 13:25:53 -0300

    Delete Atividade18_09/Atividade18_09 directory

--- Atividade18_09/Atividade18_09/Exercicio1.java
+++
@@ -1,9 +0,0 @@
-package Atividade18_09;
-
-public class Exercicio1 {
-    public static void main(String[] args) {
-        for (int i = 6; i <= 600; i += 2) { 
-            System.out.println(i);
-        }
-    }
-}
